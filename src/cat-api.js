a7832b000a5d646acd32e61b164157d3855c10c2
Danylo Altukhov <dan.97198@gmail.com>
2023-09-01 17:28:41 +0300

    add api key

---
+++ src/cat-api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const KEY =
+  'live_1xQ7G3lHO6kCrYUsXMIP4c1u8x2P4U3kassjlwcpGW5u6Hl3j5rz01HACjREbubp';
+
+axios.defaults.headers.common['x-api-key'] = KEY;
+
+const fetchCatBreeds = () => {};
+const fetchCatById = catId => {};
